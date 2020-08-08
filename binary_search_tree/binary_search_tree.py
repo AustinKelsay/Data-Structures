0417337b39f311d84549257ddf714a8200e9de05
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-08-08 06:36:58 -0500

    Got BST working without stretch

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,37 +19,88 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        cur = self
+        while cur is not None:
+            if cur.value > value:
+                if cur.left == None:
+                    cur.left = BSTNode(value)
+                    return cur.left.value
+                else:
+                    cur = cur.left
+            if cur.value <= value:
+                if cur.right == None:
+                    cur.right = BSTNode(value)
+                    return cur.right.value
+                else:
+                    cur = cur.right
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if (self.value == target):
+            return True
+        if (self.value > target):
+            if (self.left == None):
+                return False
+            if (self.left.value == target):
+                return True
+            else:
+                return self.left.contains(target)
+        if (self.value <= target):
+            if (self.right == None):
+                return False
+            if (self.right.value == target):
+                return True
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if (self.right == None):
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if (self.right):
+            self.right.for_each(fn)
+        if (self.left):
+            self.left.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        pass
+    def in_order_print(self, node):
+        if (node.left):
+            node.left.in_order_print(node.left)
+        print(node.value)
+        if (node.right):
+            node.right.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self):
-        pass
+    def bft_print(self, node):
+        queue = []
+        queue.append(node)
+        while (queue):
+            current_node = queue.pop(0)
+            print(current_node.value)
+            if (current_node.left):
+                queue.append(current_node.left)
+            if (current_node.right):
+                queue.append(current_node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self):
-        pass
+    def dft_print(self, node):
+        print(node.value)
+        if (node.left):
+            node.left.dft_print(node.left)
+        if (node.right):
+            node.right.dft_print(node.right)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -60,26 +113,27 @@ class BSTNode:
     def post_order_dft(self):
         pass
 
+
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
-
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
-
-bst.bft_print()
-bst.dft_print()
-
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# bst = BSTNode(1)
+#
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
+#
+# bst.bft_print()
+# bst.dft_print()
+#
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()
